712f1aaa99e763f18e099ed3aca3a53a676a3dce
Meriton <meritonkryeziu0@gmail.com>
2021-01-13 15:13:49 +0100

    updated README

--- modular_madness.cpp
+++ modular_madness.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <string>
+#include <string> // Working with strings
 
-using namespace std;
+using namespace std; //Namespace std so that std::cout is just cout.
 
 int main()
 {
 
-
+    return 0;
 }
-
-v
