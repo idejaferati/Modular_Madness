63e870ef380d00bdf0abbf24b55c9e1f0a84c8bd
Meriton <meritonkryeziu0@gmail.com>
2021-01-13 15:31:53 +0100

    added interacite_command()

--- modular_madness.cpp
+++ modular_madness.cpp
@@ -3,8 +3,60 @@
 
 using namespace std; //Namespace std so that std::cout is just cout.
 
+void interactive_command();
+
 int main()
 {
-
+    interactive_command(); // Calls interactive_command() with a infinte loop that exits when the given commad is exit.
     return 0;
+}
+
+void interactive_command()
+{
+    string command;
+
+    do {        
+        cin >> command;
+
+        if (command == "echo")
+        {
+            //TODO
+
+
+            // cout << "echo" << endl; // Prints echo when called, used in early testing
+        }
+        else if (command == "process")
+        {
+            //TODO
+
+
+
+            // cout << "process"<<endl; 
+        }
+        else if (command == "reverse")
+        {
+            //TODO
+
+
+            
+            // cout << "reverse"<<endl; 
+        }
+        else if (command == "delay")
+        {
+            //TODO
+
+
+            
+            // cout << "delay"<<endl; 
+        }
+        else if (command == "noop")
+        {
+            //TODO
+
+
+            
+            // cout << "noop"<<endl; 
+        }
+
+    } while (command != "exit");
 }
