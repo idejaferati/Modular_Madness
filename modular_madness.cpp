3a0459a4df10676f96f64570864d1d16aaa2c3b1
Ideja Ferati <77343818+idejaferati@users.noreply.github.com>
2021-01-26 07:11:02 -0800

    Update modular_madness.cpp

--- modular_madness.cpp
+++ modular_madness.cpp
@@ -10,11 +10,11 @@ using namespace std; //Namespace std so that std::cout is just cout.
 pair<string, string> module;
 vector<pair<string, string>> modules;
 vector<string> words;
+vector<pair<string, string>> processedStrings;
 
 string echo(string word)
 {
-
-    return word + word;
+    return word+word;
 }
 
 string reverse(string word)
@@ -27,18 +27,32 @@ string reverse(string word)
         swap(reverse_word[i], reverse_word[last]);
         last = last - 1;
     }
-
+    
     return reverse_word;
 }
 
 string delay(vector<string> words)
 {
-    return words[words.size() - 2];
+    return words[words.size()-1];
 }
 
 string noop(string word)
 {
     return word;
+}
+
+
+void createModule(string name, string operation) {
+
+    module.first = name;
+    module.second = operation;
+    if (modules.empty()) {
+        modules.push_back(module);
+    }
+    else {
+        modules.clear();
+        modules.push_back(module);
+    }
 }
 
 static void moduleCommandRun() {
@@ -51,11 +65,22 @@ static void moduleCommandRun() {
     createModule(nameOfModule, operation);
 }
 
-static void createModule(string name, string operation) {
 
-    module.first = name;
-    module.second = operation;
-    modules.push_back(module);
+void connectModules(string first_module, string second_module) {
+    string firstWord;
+    string secondWord;
+
+    for (pair<string, string> module : processedStrings) {
+        firstWord = module.second;
+        secondWord = module.first;
+    }
+
+    if (module.first == first_module) {
+        cout << firstWord + secondWord;
+    }
+    else {
+        cout << secondWord + firstWord;
+    }
 }
 
 void connectCommandRun() {
@@ -68,13 +93,10 @@ void connectCommandRun() {
     connectModules(first_module, second_module);
 }
 
-void connectModules(string first_module, string second_module) {
-
-}
 
 void interactive_command()
 {
-    char test[200];
+    char test[200]; 
     char test2[200];
     char test3[200];
     string command, argument1, argument2, argument3, argument4; // command expects an input like module, connect, process and exit, argumets1-4 expect strings and names or modules.
@@ -93,17 +115,21 @@ void interactive_command()
         // [process, keyword1, keyword2] -> array of process
 
 
-        char name[256], title[256];
+        //char name[256], title[256];
 
-        cout << "Enter your name: ";
-        cin.getline(name, 256);
-        cout << name;
-        cout << "Enter your favourite movie: ";
-        cin.getline(title, 256);
+        //cout << "Enter your name: ";
+        //cin.getline(name, 256);
+        //cout << name;
+        //cout << "Enter your favourite movie: ";
+        //cin.getline(title, 256);
 
-        cout << name << "'s favourite movie is " << title;
+        //cout << name << "'s favourite movie is " << title;
 
         //cin >> command >> argument1 >> argument2;//>>argument3>>argument4; //NEEDED a way to figure reading input till 'ENTER' is pressed
+        
+        cout << "Hello! Please write the command: \n";
+
+        cin >> command;
 
         if (command == "module") // module <name> <operation>
         {
@@ -116,7 +142,7 @@ void interactive_command()
         }
         else if (command == "process") // processes commands with given strings.
         {
-            //TODO
+            //TODO save the inputs
 
         }
 
@@ -130,4 +156,4 @@ int main()
 
 
 //This comes in handy when using reverse() because everycharacter needs to be indexed to an array and then called in reverse.
-//https://stackoverflow.com/questions/3955601/how-do-i-split-a-string-at-an-arbitrary-index 
+//https://stackoverflow.com/questions/3955601/how-do-i-split-a-string-at-an-arbitrary-index
